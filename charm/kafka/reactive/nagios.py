7e006f1890986eb4a25fa49efc37ee542a283cb9
kngu9 <khanhtnguyen300@gmail.com>
2019-04-17 16:18:39 +0000

    Removed alerts

--- charm/kafka/reactive/nagios.py
+++ charm/kafka/reactive/nagios.py
@@ -28,11 +28,6 @@ name=UnderReplicatedPartitions',
         'description': 'Number of under replicated partitions',
         'crit': 'val != 0',
     }, {
-        'name': 'active_controller_count',
-        'object_name': 'kafka.controller:type=KafkaController,\
-name=ActiveControllerCount',
-        'description': 'Number of active controllers in cluster'
-    }, {
         'name': 'offline_partitions_count',
         'object_name': 'kafka.controller:type=KafkaController,\
 name=OfflinePartitionsCount',
@@ -46,20 +41,6 @@ name=LeaderElectionRateAndTimeMs',
         'description': 'Leader election rate and latency in milliseconds',
         'warn': 'val >= {}'.format(config['nagios_leader_election_rate_warn']),
         'crit': 'val >= {}'.format(config['nagios_leader_election_rate_crit'])
-    }, {
-        'name': 'producer_time',
-        'object_name': 'kafka.network:type=RequestMetrics,\
-name=TotalTimeMs,request=Produce',
-        'attribute': '99thPercentile',
-        'description': 'The top 99th percentile total time \
-in milliseconds to produce a message'
-    }, {
-        'name': 'consumer_fetch_time',
-        'object_name': 'kafka.network:type=RequestMetrics,\
-name=TotalTimeMs,request=FetchConsumer',
-        'attribute': '99thPercentile',
-        'description': 'The top 99th percentile total time\
-in milliseconds for a consumer to fetch data'
     }, {
         'name': 'avg_network_processor_idle',
         'object_name': 'kafka.network:name=NetworkProcessorAvgIdlePercent,\
